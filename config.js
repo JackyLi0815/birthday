d44ddb732e4a5dccb2b86b7fa1a060a459475df5
JackyLi0815 <148887136+JackyLi0815@users.noreply.github.com>
2024-04-30 23:10:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ var config = {
     desc: {
         turn_on: "开始过生日",
         play: "点击播放音乐",
-        bannar_coming: "继续点击",
-        balloons_flying: "好像少点东西",
+        bannar_coming: "请继续点击",
+        balloons_flying: "唉？？好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
